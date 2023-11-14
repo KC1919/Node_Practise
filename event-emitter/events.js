cf39e5f18e3952e6cab6815ed986c144582ce537
Kunal Chandra <kunal.chandra1900@gmail.com>
2023-11-14 13:58:38 +0530

    added prettier config file

--- event-emitter/events.js
+++ event-emitter/events.js
@@ -3,20 +3,19 @@ import httpServer from 'http';
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on('message', name => {
+myEmitter.on('message', (name) => {
     console.log(`Received a new message from: ${name}`);
-})
+});
 
-myEmitter.emit('message', "Kunal");
-
+myEmitter.emit('message', 'Kunal');
 const server = httpServer.createServer();
 
 server.on('request', (req, res) => {
     console.log(req.url);
     res.setHeader('Content-Type', 'text/html');
-    res.end("<h5>Hello from home!</h5>");
-})
+    res.end('<h5>Hello from home!</h5>');
+});
 
 server.listen(3000, () => {
-    console.log("Listening on port 3000");
-})
+    console.log('Listening on port 3000');
+});
